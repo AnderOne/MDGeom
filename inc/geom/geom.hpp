d8bc6bf66257d76bfe072a4a60f88396e5c66b6a
Ander <armath123@gmail.com>
2020-05-24 00:42:18 +0300

    Add LICENSE

--- inc/geom/geom.hpp
+++ inc/geom/geom.hpp
@@ -1,2 +1,23 @@
+/**
+ * Copyright (c) 2019-2020 Andrey Baranov <armath123@gmail.com>
+ *
+ * This file is part of MDGeom (Multi-Dimensional Geometry).
+ *
+ * MDGeom is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MDGeom is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MDGeom;
+ * if not, see <http://www.gnu.org/licenses/>
+**/
+
+#pragma once
 #include "base.hpp"
 #include "mesh.hpp"
