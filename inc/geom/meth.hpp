2399760d66466ce1323593e6a3d7785c5d782e5c
Ander <armath123@gmail.com>
2020-10-29 16:21:12 +0300

    Update and fix for slicing and clipping algorithms

--- inc/geom/meth.hpp
+++ inc/geom/meth.hpp
@@ -54,90 +54,138 @@ auto getProject(const t_mesh<T, N, M> &mesh, const t_basis<T, N, N - 1> &basis) 
 	};
 }
 
-template <unsigned N, unsigned M, unsigned K, bool IS_SLICE = false> struct t_sect_builder {
+enum t_state { CROSS, LOWER, UPPER, INNER };
+typedef std::vector<int> t_child;
 
-	t_sect_builder(const t_grid<N> &_old_grid, t_grid<M> &_new_grid):
-	               old_grid(_old_grid), new_grid(_new_grid) {}
+template <unsigned N, unsigned M, unsigned K, bool IS_SLICE = false>
+struct t_sect_builder {
 
-	void make_new_cell(const std::vector<int> &old_item_index,
-	                   const std::vector<int> &new_cell_child,
-	                   std::vector<int> &old_cell_index) {
+	t_sect_builder(const t_grid<N> &_old_grid, t_grid<M> &_new_grid): old_grid(_old_grid), new_grid(_new_grid) {}
+
+	void make_new_item(const std::vector<t_state> &old_item_state,
+	                   const std::vector<t_child> &new_item_child,
+	                   const std::vector<int> &new_item_index,
+	                   std::vector<t_state> &old_cell_state,
+	                   std::vector<t_child> &new_cell_child,
+	                   std::vector<int> &new_cell_index) {
+
+		const auto &old_cell = old_grid.template cell<K + 1>();
+		auto &new_item = new_grid.template cell<K>();
+
+		for (int i = 0; i < old_cell.size(); ++ i) {
+
+			//Проверяем положение ячейки относительно подпространства:
+			size_t cross = 0, lower = 0, upper = 0, inner = 0;
+			for (int c: old_cell[i]) {
+				if (old_item_state[c] == CROSS) { ++ cross; }
+				if (old_item_state[c] == LOWER) { ++ lower; }
+				if (old_item_state[c] == UPPER) { ++ upper; }
+				if (old_item_state[c] == INNER) {
+					assert(new_item_index[c] != nullind);
+					new_cell_child[i].
+					push_back(new_item_index[c]);
+					++ inner;
+				}
+			}
+			if (inner == old_cell[i].size())
+				old_cell_state[i] = INNER;
+			else
+			if (cross || (upper && lower))
+				old_cell_state[i] = CROSS;
+			else {
+				old_cell_state[i] =
+				lower?
+				LOWER: UPPER;
+			}
+
+			if (old_cell_state[i] != CROSS) continue;
+
+			//Добавляем новую подъячейку:
+			std::set<int> item;
+			for (int c: old_cell[i]) for (int k: new_item_child[c]) item.insert(k);
+			t_push<K> push;
+			for (int k: item) push.add(k);
+
+			assert(push.pos > K);
+
+			new_cell_child[i].push_back(
+				new_item.size()
+			);
+			new_item.push_back(
+			push.item
+			);
+		}
+	}
+
+	void make_new_cell(const std::vector<t_state> &old_item_state,
+	                   const std::vector<t_child> &new_item_child,
+	                   const std::vector<int> &new_item_index,
+	                   std::vector<t_state> &old_cell_state,
+	                   std::vector<t_child> &new_cell_child,
+	                   std::vector<int> &new_cell_index) {
 
 		const auto &old_cell = old_grid.template cell<K + 1>();
 		auto &new_cell = new_grid.template cell<K + 1>();
 
 		for (int i = 0; i < old_cell.size(); ++ i) {
+
+			if (!IS_SLICE) {
+				if ((old_cell_state[i] != INNER) && (old_cell_state[i] != UPPER) && (old_cell_state[i] != CROSS)) continue;
+			}
+			else {
+				if (old_cell_state[i] != INNER) continue;
+			}
+
+			std::set<int> item;
+			if (!IS_SLICE) for (int k: new_cell_child[i]) item.insert(k);
+			for (int c: old_cell[i]) {
+			int k = new_item_index[c]; if (k != nullind) item.insert(k);
+			}
 			t_cell<K + 1> cell;
-			for (int c: old_cell[i]) {
-				int k = old_item_index[c]; if (k != nullind) cell.push_back(k);
-			}
-			bool comp = (cell.size() == old_cell[i].size());
-			int k = new_cell_child[i];
-			if (!IS_SLICE && (k != nullind)) {
-				cell.push_back(k);
-				comp = true;
-			}
-			if (comp) {
-				old_cell_index[i] =
-				new_cell.size();
-				new_cell.push_back(
-				cell
-				);
-			}
+			for (int k: item) cell.push_back(k);
+
+			//Добавляем новую ячейку:
+			new_cell_index[i] =
+			new_cell.size();
+			new_cell.push_back(
+			cell
+			);
 		}
 	}
 
-	void make_new_item(const std::vector<int> &new_item_child, std::vector<int> &new_cell_child) {
+	void make(const std::vector<t_state> &old_item_state,
+	          const std::vector<t_child> &new_item_child,
+	          const std::vector<int> &new_item_index) {
 
 		const auto &old_cell = old_grid.template cell<K + 1>();
 		auto &new_item = new_grid.template cell<K>();
 
-		for (int i = 0; i < old_cell.size(); ++ i) {
-			t_push<K> push;
-			for (int c: old_cell[i]) {
-				int k = new_item_child[c]; if (k != nullind) push.add(k);
-			}
-			if (push.pos > K) {
-				new_cell_child[i] =
-				new_item.size();
-				new_item.push_back(
-				push.item
-				);
-			}
-		}
-	}
-
-	void make(const std::vector<int> &old_item_index, const std::vector<int> &new_item_child) {
-
-		const auto &old_cell = old_grid.template cell<K + 1>();
-		auto &new_item = new_grid.template cell<K>();
-
-		std::vector<int> old_cell_index(
-			old_cell.size(), nullind
-		);
-		std::vector<int> new_cell_child(
+		std::vector<t_state> old_cell_state(old_cell.size());
+		std::vector<t_child> new_cell_child(old_cell.size());
+		std::vector<int> new_cell_index(
 			old_cell.size(), nullind
 		);
 
 		//Заполняем новые подъячейки меньшей размерности:
 		make_new_item(
-			new_item_child, new_cell_child
+			old_item_state, new_item_child, new_item_index,
+			old_cell_state, new_cell_child, new_cell_index
 		);
 		//Заполняем новые ячейки из подъячеек:
-		t_sect_builder<N, M, (K < M)? (K): (N), IS_SLICE>(old_grid, new_grid).make_new_cell(
-			old_item_index, new_cell_child,
-			old_cell_index
+		t_sect_builder<N, M, (K < M)? (K): (N), IS_SLICE>
+		(old_grid, new_grid).make_new_cell(
+			old_item_state, new_item_child, new_item_index,
+			old_cell_state, new_cell_child, new_cell_index
 		);
 		//Вызываемся рекурсивно вверх:
-		t_sect_builder<N, M, K + 1, IS_SLICE>(old_grid, new_grid).make(
-			old_cell_index,
-			new_cell_child
+		t_sect_builder<N, M, K + 1, IS_SLICE>
+		(old_grid, new_grid).make(
+			old_cell_state, new_cell_child, new_cell_index
 		);
 
 		//Заполняем обратные ссылки:
 		t_hand<M, K>::fill(
-			new_grid,
-			new_item
+		new_grid, new_item
 		);
 	}
 
@@ -165,7 +213,7 @@ auto getClipped(const t_mesh<T, N, M> &mesh, const t_vector<T, N> &center,
 	//Разделяем вершины относительно подпространства:
 	const auto &old_vert = mesh.vert(); std::vector<t_vert<T, N>> new_vert;
 
-	std::vector<int> old_vert_index(old_vert.size(), nullind);
+	std::vector<int> new_vert_index(old_vert.size(), nullind);
 	std::vector<int> old_vert_state(old_vert.size());
 
 	for (int i = 0; i < old_vert.size(); ++ i) {
@@ -173,13 +221,13 @@ auto getClipped(const t_mesh<T, N, M> &mesh, const t_vector<T, N> &center,
 		const T p = (old_vert[i] - center) * normal;
 
 		if (p == 0) {
-			old_vert_index[i] = new_vert.size();
+			new_vert_index[i] = new_vert.size();
 			new_vert.push_back(old_vert[i]);
 			old_vert_state[i] = +0;
 		}
 		else
 		if (p > 0) {
-			old_vert_index[i] = new_vert.size();
+			new_vert_index[i] = new_vert.size();
 			new_vert.push_back(old_vert[i]);
 			old_vert_state[i] = +1;
 		}
@@ -194,65 +242,72 @@ auto getClipped(const t_mesh<T, N, M> &mesh, const t_vector<T, N> &center,
 	const auto &old_edge = old_grid.template cell<1>();
 	auto &new_edge = new_grid.template cell<1>();
 
-	std::vector<int> old_edge_index(old_edge.size(), nullind);
-	std::vector<int> new_edge_child(old_edge.size(), nullind);
+	std::vector<t_state> old_edge_state(old_edge.size());
+	std::vector<t_child> new_edge_child(old_edge.size());
+	std::vector<int> new_edge_index(
+		old_edge.size(), nullind
+	);
 
 	for (int i = 0; i < old_edge.size(); ++ i) {
 
-		const auto &edge = old_edge[i];
-		const int a = edge[0];
-		const int b = edge[1];
+		const auto &edge = old_edge[i]; const int a = edge[0], b = edge[1];
 
-		if (old_vert_state[a] * old_vert_state[b] < 0) {
-			const auto &pa = old_vert[a];
-			const auto &pb = old_vert[b];
+		if (old_vert_state[a] == 0) {
+			new_edge_child[i].push_back(new_vert_index[a]);
+		}
+		if (old_vert_state[b] == 0) {
+			new_edge_child[i].push_back(new_vert_index[b]);
+		}
+		if (old_vert_state[a] * old_vert_state[b] < 0)
+			old_edge_state[i] = CROSS;
+		else
+		if (old_vert_state[a] + old_vert_state[b] < 0)
+			old_edge_state[i] = LOWER;
+		else
+		if (old_vert_state[a] + old_vert_state[b] > 0)
+			old_edge_state[i] = UPPER;
+		else {
+			old_edge_state[i] = INNER;
+		}
+
+		if (old_edge_state[i] == CROSS) {
+			const auto &pa = old_vert[a], &pb = old_vert[b];
 			T p = - ((pa - center) * normal) /
 			        ((pb - pa) * normal);
 			//Add new vert:
-			new_edge_child[i] = new_vert.size();
+			new_edge_child[i].push_back(new_vert.size());
 			new_vert.push_back(
 			pa + p * (pb - pa)
 			);
-			//Add new edge:
-			old_edge_index[i] = new_edge.size();
-			int va = (old_vert_state[a] >= 0)?
-			          old_vert_index[a]:
-			          new_edge_child[i];
-			int vb = (old_vert_state[b] >= 0)?
-			          old_vert_index[b]:
-			          new_edge_child[i];
-			new_edge.push_back({
-				va, vb
-			});
+			//Add new edge (short of edge):
+			new_edge_index[i] = new_edge.size();
+			int va = (old_vert_state[a] > 0)?
+			          new_vert_index[a]:
+			          new_vert.size() - 1;
+			int vb = (old_vert_state[b] > 0)?
+			          new_vert_index[b]:
+			          new_vert.size() - 1;
+			new_edge.push_back({va, vb});
 		}
-		else
-		if ((old_vert_state[a] >= 0) &&
-		    (old_vert_state[b] >= 0)) {
-			//Случай, когда ребро одним концом касается гиперплоскости!
-			//Add new vert:
-			if (!old_vert_state[a] && old_vert_state[b]) {
-				new_edge_child[i] = old_vert_index[a];
-			}
-			if (!old_vert_state[b] && old_vert_state[a]) {
-				new_edge_child[i] = old_vert_index[b];
-			}
+		if (old_edge_state[i] == INNER ||
+		    old_edge_state[i] == UPPER) {
 			//Add new edge:
-			old_edge_index[i] = new_edge.size();
+			new_edge_index[i] = new_edge.size();
 			new_edge.push_back({
-			old_vert_index[a],
-			old_vert_index[b]
+			new_vert_index[a],
+			new_vert_index[b]
 			});
 		}
 	}
 
 	//Вызываемся рекурсивно вверх:
-	t_sect_builder<M, M, 1>(old_grid, new_grid
-	).make(
-	old_edge_index, new_edge_child
+	t_sect_builder<M, M, 1>(old_grid, new_grid).make(
+	old_edge_state, new_edge_child, new_edge_index
 	);
 
 	return t_mesh<T, N, M>(
-	std::move(new_vert), new_grid
+	std::move(new_vert),
+	new_grid
 	);
 }
 
@@ -273,7 +328,7 @@ auto getSection(const t_mesh<T, N, M> &mesh, const t_basis<T, N, N - 1> &basis) 
 	const auto &old_vert = mesh.vert();
 	std::vector<t_sliced_vert> new_vert;
 
-	std::vector<int> old_vert_index(old_vert.size(), nullind);
+	std::vector<int> new_vert_index(old_vert.size(), nullind);
 	std::vector<int> old_vert_state(old_vert.size());
 
 	for (int i = 0; i < old_vert.size(); ++ i) {
@@ -281,17 +336,15 @@ auto getSection(const t_mesh<T, N, M> &mesh, const t_basis<T, N, N - 1> &basis) 
 		const T p = (old_vert[i] - center) * normal;
 
 		if (p == 0) {
-			old_vert_index[i] = new_vert.size();
+			new_vert_index[i] = new_vert.size();
 			new_vert.push_back(
-			basis.put(old_vert[i])
+				basis.put(old_vert[i])
 			);
 			old_vert_state[i] = +0;
 		}
-		else
 		if (p > 0) {
 			old_vert_state[i] = +1;
 		}
-		else
 		if (p < 0) {
 			old_vert_state[i] = -1;
 		}
@@ -302,35 +355,50 @@ auto getSection(const t_mesh<T, N, M> &mesh, const t_basis<T, N, N - 1> &basis) 
 	const auto &old_edge = old_grid.template cell<1>();
 	auto &new_edge = new_grid.template cell<1>();
 
-	std::vector<int> old_edge_index(old_edge.size(), nullind);
-	std::vector<int> new_edge_child(old_edge.size(), nullind);
+	std::vector<t_state> old_edge_state(old_edge.size());
+	std::vector<t_child> new_edge_child(old_edge.size());
+	std::vector<int> new_edge_index(
+		old_edge.size(), nullind
+	);
 
 	for (int i = 0; i < old_edge.size(); ++ i) {
 
-		const auto &edge = old_edge[i];
-		const int a = edge[0];
-		const int b = edge[1];
+		const auto &edge = old_edge[i]; const int a = edge[0], b = edge[1];
 
-		if (old_vert_state[a] * old_vert_state[b] < 0) {
-			const auto &pa = old_vert[a];
-			const auto &pb = old_vert[b];
+		if (old_vert_state[a] == 0) {
+			new_edge_child[i].push_back(new_vert_index[a]);
+		}
+		if (old_vert_state[b] == 0) {
+			new_edge_child[i].push_back(new_vert_index[b]);
+		}
+		if (old_vert_state[a] * old_vert_state[b] < 0)
+			old_edge_state[i] = CROSS;
+		else
+		if (old_vert_state[a] + old_vert_state[b] < 0)
+			old_edge_state[i] = LOWER;
+		else
+		if (old_vert_state[a] + old_vert_state[b] > 0)
+			old_edge_state[i] = UPPER;
+		else {
+			old_edge_state[i] = INNER;
+		}
+
+		if (old_edge_state[i] == CROSS) {
+			const auto &pa = old_vert[a], &pb = old_vert[b];
 			T p = - ((pa - center) * normal) /
 			        ((pb - pa) * normal);
 			//Add new vert:
-			new_edge_child[i] = new_vert.size();
+			new_edge_child[i].push_back(new_vert.size());
 			new_vert.push_back(
-			basis.put(
-			pa + p * (pb - pa))
+			basis.put(pa + p * (pb - pa))
 			);
 		}
-		else
-		if ((old_vert_state[a] == 0) &&
-		    (old_vert_state[b] == 0)) {
+		if (old_edge_state[i] == INNER) {
 			//Add new edge:
-			old_edge_index[i] = new_edge.size();
+			new_edge_index[i] = new_edge.size();
 			new_edge.push_back({
-			old_vert_index[a],
-			old_vert_index[b]
+			new_vert_index[a],
+			new_vert_index[b]
 			});
 		}
 	}
@@ -339,11 +407,12 @@ auto getSection(const t_mesh<T, N, M> &mesh, const t_basis<T, N, N - 1> &basis) 
 	constexpr unsigned L = t_sect_reduce<N, M>::dim;
 	t_sect_builder<M, L, 1, true>(old_grid, new_grid
 	).make(
-	old_edge_index, new_edge_child
+	old_edge_state, new_edge_child, new_edge_index
 	);
 
 	return t_sliced_mesh(
-	std::move(new_vert), new_grid
+	std::move(new_vert),
+	new_grid
 	);
 }
 
